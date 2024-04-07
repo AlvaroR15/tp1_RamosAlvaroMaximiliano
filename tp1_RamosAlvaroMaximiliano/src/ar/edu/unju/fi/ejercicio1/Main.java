86178341601c70d9d5aecff8c025bdc7a300f0d6
AlvaroR15 <alvaroramos048@gmail.com>
2024-04-07 20:40:09 -0300

    Ejercicio 1 - Uso de variables: terminado.

---
+++ tp1_RamosAlvaroMaximiliano/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		float numUno = 8;
+		float numDos = 5;
+		float numTres = 1;
+		float numCuatro = 20;
+		float numCinco = 9;
+		
+		float promedio = (numUno+numDos+numTres+numCuatro+numCinco) / 5;
+		System.out.println("El promedio es: " + promedio);
+	}
+}
