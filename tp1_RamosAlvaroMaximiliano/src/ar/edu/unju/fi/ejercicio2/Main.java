1a484c395648cde6bed2b82d42245117a0ee7399
AlvaroR15 <alvaroramos048@gmail.com>
2024-04-07 21:04:19 -0300

    Ejercicio 2 - Uso de variables: terminado.

---
+++ tp1_RamosAlvaroMaximiliano/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "Argentina";
+		int edadPersona = 22;
+		float alturaEdificio = 15.7f;
+		float precioProducto = 89.99f;
+		String numTelefono = "3881234567";
+		double coseno = Math.cos(0.5);
+		
+		System.out.println("El país es: " + pais);
+		System.out.println("La edad de la persona es: " + edadPersona);
+		System.out.println("La altura del edificio es: " + alturaEdificio);
+		System.out.println("El precio del producto de supermercado es: " + precioProducto);
+		System.out.println("El número de teléfono es: " + numTelefono);
+		System.out.println("El cálculo de coseno de 0.5 es: " + coseno);
+
+	}	
+
+}
